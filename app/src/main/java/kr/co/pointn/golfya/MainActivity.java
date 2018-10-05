0493e8fb3c97ccfe75d983362b222a675558454b
MiSamChoi <cms736@naver.com>
2018-10-05 17:58:58 +0900

    1

---
+++ app/src/main/java/kr/co/pointn/golfya/MainActivity.java
@@ -0,0 +1,196 @@
+package kr.co.pointn.golfya;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.List;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private WebView mWebView;
+    final AppCompatActivity activity = this;
+
+    private static  int networkYn = 0;
+    private String myUrl = "http://www.sacoop.kr/?token="; // 접속 URL (내장HTML의 경우 왼쪽과 같이 쓰고 아니면 걍 URL)
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        if (token != null) {
+            Log.d(TAG, "token = " + token);
+
+
+
+
+        }
+        updateIconBadge(activity,  0);
+// 웹뷰 셋팅팅
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        Online();
+        if(networkYn==2){
+
+            NotOnline();
+        }else {
+
+            mWebView.loadUrl(myUrl + token); // 접속 URL
+            mWebView.setWebChromeClient(new WebChromeClient());
+            mWebView.setWebViewClient(new WebViewClientClass());
+
+        }
+
+        // 키보드 내리기
+        //InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        // imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+
+        Log.d(TAG, "페이지이동 ");
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    @Override
+    public void onBackPressed() {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyAlertDialogStyle);
+
+        builder.setIcon(R.drawable.sacoop_icon);
+        builder.setTitle(R.string.app_name);
+        builder.setMessage(R.string.exitmsg);
+        builder.setPositiveButton(R.string.exitmsgY, new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                });
+        builder.setNegativeButton(R.string.exitmsgN, null);
+        AlertDialog dialog = builder.show();
+
+
+    }
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            Online();
+            if(networkYn==2){
+
+                NotOnline();
+                return true;
+
+            }else {
+
+                Log.d("check URL", url);
+                view.loadUrl(url);
+                return super.shouldOverrideUrlLoading(view, url);
+
+
+            }
+        }
+    }
+    public static String getLauncherClassName(Context context) {
+        PackageManager pm = context.getPackageManager();
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
+        for (ResolveInfo resolveInfo : resolveInfos) {
+            String pkgName = resolveInfo.activityInfo.applicationInfo.packageName;
+            if (pkgName.equalsIgnoreCase(context.getPackageName())) {
+                String className = resolveInfo.activityInfo.name;
+                return className;
+            }
+        }
+        return null;
+    }
+
+    public static void updateIconBadge(Context context, int notiCnt) {
+        Intent badgeIntent = new Intent("android.intent.action.BADGE_COUNT_UPDATE");
+        badgeIntent.putExtra("badge_count", notiCnt);
+        badgeIntent.putExtra("badge_count_package_name", context.getPackageName());
+        badgeIntent.putExtra("badge_count_class_name", getLauncherClassName(context));
+        context.sendBroadcast(badgeIntent);
+    }
+    public void NotOnline() {
+        final String networkmsg = getString(R.string.networkmsg);
+
+        //mWebView.loadUrl("javascript:alert('"+networkmsg+"')");
+
+
+
+        new AlertDialog.Builder(this, R.style.MyAlertDialogStyle)
+                .setIcon(R.drawable.sacoop_icon)
+                .setTitle(R.string.app_name)
+                .setMessage(""+networkmsg+"")
+                .setNegativeButton(R.string.exitmsgN, null)
+                .setPositiveButton(R.string.exitmsgY,new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog,int whichButton)
+                    {
+                        finish();
+                        overridePendingTransition(R.anim.zoom_enter, R.anim.zoom_exit);
+                    }
+                }).show();
+
+
+       // startActivity(new Intent(getApplicationContext(), OfflineActivity.class));
+
+
+    }
+    public void Online() {
+        ConnectivityManager manager = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mobile = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo wifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+
+        // wifi 또는 모바일 네트워크 어느 하나라도 연결이 되어있다면,
+        if (wifi.isConnected() || mobile.isConnected()) {
+
+            Log.d("연결됨" , "연결이 되었습니다.");
+            networkYn =1;
+
+
+        } else {
+            Log.d("연결 안 됨" , "연결이 다시 한번 확인해주세요");
+            networkYn =2;
+
+        }
+    }
+
+
+
+}
